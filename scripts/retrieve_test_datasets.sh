25f9894c3cbf618fc4405a3d325875f0c8e6d2e1
hayssam <hayssam.soueidan@displayce.com>
2020-04-14 15:13:05 +0200

    minor(setup) : Sample datasets downloader

--- scripts/retrieve_test_datasets.sh
+++ scripts/retrieve_test_datasets.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-curl https://services.cbib.u-bordeaux.fr//prestations/ProteomX_sprint/ProteomX_sprint_rawData.xlsx -o
+curl https://services.cbib.u-bordeaux.fr//prestations/ProteomX_sprint/ProteomX_sprint_rawData.xlsx -o data/ProteomX_sprint_rawData.xlsx
+curl https://services.cbib.u-bordeaux.fr//prestations/ProteomX_sprint/mapping_ProteomX_sprint_rawData.csv -o data/mapping_ProteomX_sprint_rawData.csv
