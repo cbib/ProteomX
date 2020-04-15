2eff804ef5ce8eb92c33b09e1376060d86a8710c
hayssam <hayssam.soueidan@displayce.com>
2020-04-15 20:04:04 +0200

    minor(setup): Added sample hello logger for testing + fixing PYTHONPATH

--- backend/hello.py
+++ backend/hello.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Credits: Claire Lescoat, Macha Nikolski, Benjamin Dartigues, Cedric Usureau, Aurélien Barré, Hayssam Soueidan
+import argparse
 import sys
 import uuid
 
@@ -14,8 +15,22 @@ config = {
     "extra": {"job_uuid": uuid.uuid4()}
 }
 logger.configure(**config)
+
+
+def get_args():
+    example_text = "Usage example: " \
+                   "hello.py --name Claire Macha Ben Cedric Aurélien Hayssam"
+    parser = argparse.ArgumentParser(epilog=example_text)
+    parser.add_argument('--name', nargs='+', required=False, help='A list of names to greet', default=["world"])
+    args = parser.parse_args()
+    return args
+
+
+def say_hello(name):
+    logger.bind(result={"status": "all right", "target": name, "result": f"hello {name}"}).info(f"This is a message for {name} with extra data")
+
+
 if __name__ == '__main__':
-    logger.info("Hello world")
-    logger.info("This is another message")
-    logger.error("This is an error!")
-    logger.bind(result={"status": "all right"}).info("This is a message with extra data")
+    args = get_args()
+    for n in args.name:
+        say_hello(n)
