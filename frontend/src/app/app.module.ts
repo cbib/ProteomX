4a037b4f26581ce06b50d00652efdd896516be36
benjamin <benjamin.dartigues@u-bordeaux.fr>
2020-04-15 15:33:33 +0200

    remove import cause module angular cdk and others are not present

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,21 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DownloadComponent } from './download/download.component';
+//import { DownloadComponent } from './download/download.component';
 import { HomeComponent } from './home/home.component';
-import { DragDropModule} from '@angular/cdk/drag-drop';
-import { DataTablesModule } from 'angular-datatables';
-@NgModule({
+//import { DragDropModule} from '@angular/cdk/drag-drop';
+//import { DataTablesModule } from 'angular-datatables';
+
+    @NgModule({
   declarations: [
     AppComponent,
-    DownloadComponent,
-    HomeComponent,
+    //DownloadComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    DataTablesModule,
-    DragDropModule
+    AppRoutingModule
+    //DataTablesModule,
+    //DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
