bbc408e4eb93b291a4a32aca80b6c05692e959b0
cedricusureau <cedric.usureau@gmail.com>
2020-04-16 17:31:31 +0200

    (minor) Fix config_file.py. More error function

--- backend/make_config_file.py
+++ backend/make_config_file.py
@@ -14,8 +14,11 @@ def get_args():
     parser.add_argument("--organism", "-org", type=str, choices=["hsapiens", "mmusculus"], help="Choose organism",
                         default="hsapiens")
     parser.add_argument("--group", "-gr", nargs="+", default="group1 group2")
-    parser.add_argument("--max_na_percent", "-na", type=int, help="remove protein with less than max_na_percent",
+    parser.add_argument("--max_na_percent_prot", "-nap", type=int, help="remove protein with less than max_na_percent",
                         default=33)
+    parser.add_argument("--max_na_percent_sample", "-nas", type=int,
+                        help="remove samples with less than max_na_percent",
+                        default=80)
     parser.add_argument("--sheet_name_index", "-sh", type=str, help="sheetname index from xlsx file", default=1)
     parser.add_argument("--reference", "-ref", type=int, help="give the index of reference group", default=0)
     parser.add_argument("--output_file", "-o", help='output json file', default="backend/test/new_config_file.json")
@@ -26,5 +29,6 @@ def get_args():
 if __name__ == '__main__':
     args = get_args()
     init_fonction.write_config_file(input=args.json_template, organism=args.organism, group=args.group,
-                                    max_na=args.max_na_percent, sheet_index=args.sheet_name_index,
+                                    max_na_prot=args.max_na_percent_prot, max_na_sample=args.max_na_percent_sample,
+                                    sheet_index=args.sheet_name_index,
                                     reference=args.reference, output=args.output_file)
