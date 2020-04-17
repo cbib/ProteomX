94a0fde308efdc577fbfd11dcde8b22e6db06804
benjamin <benjamin.dartigues@u-bordeaux.fr>
2020-04-17 18:06:52 +0200

    add frontend backend to get sample names

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,23 +5,64 @@ import { AppComponent } from './app.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { DownloadComponent } from './download/download.component';
 import { HomeComponent } from './home/home.component';
-import { DragDropModule} from '@angular/cdk/drag-drop';
 import { DataTablesModule } from 'angular-datatables';
 import { DelimitorDialogComponent } from './dialog/delimitor-dialog.component';
-import { MatDialogModule } from '@angular/material/dialog';
 import { JwtInterceptor, ErrorInterceptor } from './helpers';
 import { platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NoopAnimationsModule, BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MiddlewareService} from './services/middleware.service';
 import { AlertService} from './services/alert.service';
 import { ReactiveFormsModule,FormsModule }    from '@angular/forms';
 import { MetadataDescriptionComponent } from './analysis/metadata-description.component';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
 import { HistoryComponent } from './users/history.component';
 import { AlertModule } from './modules/alert.module';
 import { ExplorationComponent } from './exploration/exploration.component';
 import { QualityCheckComponent } from './quality-check/quality-check.component';
+
+import {A11yModule} from '@angular/cdk/a11y';
+import {ClipboardModule} from '@angular/cdk/clipboard';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {PortalModule} from '@angular/cdk/portal';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+import {CdkStepperModule} from '@angular/cdk/stepper';
+import {CdkTableModule} from '@angular/cdk/table';
+import {CdkTreeModule} from '@angular/cdk/tree';
+
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatSortModule} from '@angular/material/sort';
+import {MatTableModule} from '@angular/material/table';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatTreeModule} from '@angular/material/tree';
+import { StepperOverviewComponent } from './stepper-overview/stepper-overview.component';
 
     @NgModule({
   declarations: [
@@ -33,6 +74,7 @@ import { QualityCheckComponent } from './quality-check/quality-check.component';
     HistoryComponent,
     ExplorationComponent,
     QualityCheckComponent,
+    StepperOverviewComponent,
     
   ],
   imports: [
@@ -40,14 +82,54 @@ import { QualityCheckComponent } from './quality-check/quality-check.component';
     AppRoutingModule,
     DataTablesModule,
     DragDropModule,
-    MatDialogModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
     FormsModule,
     HttpClientModule,
+    AlertModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
     MatButtonModule,
+    MatButtonToggleModule,
     MatCardModule,
-    AlertModule
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule,
+    A11yModule,
+    ClipboardModule,
+    CdkStepperModule,
+    CdkTableModule,
+    CdkTreeModule,
+    PortalModule,
+    ScrollingModule,
+    NoopAnimationsModule,
     
   ],
   entryComponents:[DelimitorDialogComponent],
@@ -58,3 +140,6 @@ import { QualityCheckComponent } from './quality-check/quality-check.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
